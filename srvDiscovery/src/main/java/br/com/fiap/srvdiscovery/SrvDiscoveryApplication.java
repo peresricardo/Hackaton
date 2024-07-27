a8a65dfda23fd7e1a11164f23a61b9d82d694ccd
ricardo peres <ricardo.peres2013@gmail.com>
2024-07-26 22:01:22 -0300

    Primeiro commit

---
+++ srvDiscovery/src/main/java/br/com/fiap/srvdiscovery/SrvDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.srvdiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrvDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SrvDiscoveryApplication.class, args);
+    }
+
+}
---
+++ srvGateway/src/main/java/br/com/fiap/srvgateway/SrvGatewayApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.srvgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrvGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SrvGatewayApplication.class, args);
+    }
+
+}
