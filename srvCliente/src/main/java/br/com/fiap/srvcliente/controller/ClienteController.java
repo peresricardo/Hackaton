0117b844d8a391bd2185ef80eaf0e9496b524fca
ricardo peres <ricardo.peres2013@gmail.com>
2024-07-31 18:09:19 -0300

    Inclusão do Swagger no serviço de cliente

--- srvCliente/src/main/java/br/com/fiap/srvcliente/controller/ClienteController.java
+++ srvCliente/src/main/java/br/com/fiap/srvcliente/controller/ClienteController.java
@@ -2,6 +2,8 @@ package br.com.fiap.srvcliente.controller;
 
 import br.com.fiap.srvcliente.dto.ClienteDto;
 import br.com.fiap.srvcliente.service.ClienteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -21,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/cliente")
+@Tag(name = "Cadastro de Clientes", description = "Endpoint para manutenção no cadastro de clientes")
 public class ClienteController {
     private final ClienteService service;
 
@@ -29,18 +32,21 @@ public class ClienteController {
     }
 
     @PostMapping
+    @Operation(summary = "Efetua a inclusão de um novo cliente", method = "POST")
     public ResponseEntity<ClienteDto> cadastrar(@Valid @RequestBody ClienteDto clienteDto) {
         var cliente = service.cadastrarCliente(clienteDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(cliente);
     }
 
     @PutMapping
+    @Operation(summary = "Efetua a alteração de um cliente existente", method = "PUT")
     public ResponseEntity<ClienteDto> alterarCliente (@Valid @RequestBody ClienteDto clienteDto) {
         var cliente = service.atualizarCliente(clienteDto);
         return ResponseEntity.status(HttpStatus.OK).body(cliente);
     }
 
     @DeleteMapping
+    @Operation(summary = "Efetua a exclusão de um cliente existente", method = "DELETE")
     public ResponseEntity<?> excluirCliente (@Valid @RequestBody ClienteDto clienteDto) {
         boolean excluido = service.excluirCliente(clienteDto);
         if (excluido) {
@@ -51,18 +57,21 @@ public class ClienteController {
     }
 
     @GetMapping("/{cpf}")
+    @Operation(summary = "Obtem um cliente cadastrado efetuando a busca por Cpf", method = "GET")
     public ResponseEntity<ClienteDto> buscarClientePorCpf(@PathVariable String cpf) {
         var cliente = service.buscarClientePorCpf(cpf);
         return ResponseEntity.status(HttpStatus.OK).body(cliente);
     }
 
     @GetMapping("/existeCliente/{cpf}")
+    @Operation(summary = "Verifica se existe um cliente cadastrado por Cpf", method = "GET")
     public ResponseEntity<Boolean> existeCliente(@PathVariable String cpf) {
         var existe = service.existeClientePorCpf(cpf);
         return ResponseEntity.status(HttpStatus.OK).body(existe);
     }
 
     @GetMapping
+    @Operation(summary = "Obtem uma lista de todos os clientes cadastrados", method = "GET")
     public ResponseEntity<Page<ClienteDto>> listarClientes(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size) {
