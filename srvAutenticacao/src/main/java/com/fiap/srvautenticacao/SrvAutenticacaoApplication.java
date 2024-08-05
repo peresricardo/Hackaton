6575e22d3ced8a8614fd481d6c508f37cd3c5940
ricardo peres <ricardo.peres2013@gmail.com>
2024-08-05 19:56:08 -0300

    Correção no serviço de autenticação

--- srvAutenticacao/src/main/java/com/fiap/srvautenticacao/SrvAutenticacaoApplication.java
+++ srvAutenticacao/src/main/java/com/fiap/srvautenticacao/SrvAutenticacaoApplication.java
@@ -2,16 +2,10 @@ package com.fiap.srvautenticacao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan(basePackages = "com.fiap.srvautenticacao.model") // Ajuste o pacote conforme necessário
-@EnableJpaRepositories(basePackages = "com.fiap.srvautenticacao.repository")
 @EnableEurekaServer
-@EnableDiscoveryClient
 public class SrvAutenticacaoApplication {
 
     public static void main(String[] args) {
